297c56b2469f388ddb745b53f119af888f96a9f7
gianluca <gianluca@HP_IDM_Gianluca.station>
2022-10-20 18:07:30 +0200

    commit importante

---
+++ ProgettoProvaGithub/src/com/corso/test/ClasseGianluca.java
@@ -0,0 +1,9 @@
+package com.corso.test;
+
+public class ClasseGianluca {
+	static String funzionebella()
+	{
+		return "salvo si incula i cani";
+	}
+
+}
--- ProgettoProvaGithub/src/com/corso/test/Test.java
+++ ProgettoProvaGithub/src/com/corso/test/Test.java
@@ -3,13 +3,8 @@ package com.corso.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Ciao belli");
-		int a = 0;
-		int b = 0;
+		System.out.println(ClasseGianluca.funzionebella());
 		
-		Object hgdhfd;
-		String alessio;
-
 	}
 
 }
